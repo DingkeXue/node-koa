bc038797b9105143f2ad47288c221b08b1ec5e5b
DingkeXue <605534423@qq.com>
2019-07-01 11:31:26 +0800

    node-ejs

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const render = require('koa-ejs');
 const app = new Koa();
 const router = new Router();
 
+let msg = [
+    {
+        name:'bar'
+    },
+    {
+        name: 'foo'
+    }
+];
+
 // 配置模板引擎
 render(app, {
     root: path.join(__dirname, 'views'),
@@ -15,11 +24,19 @@ render(app, {
 });
 
 // 路由跳转
-router.get('/', async ctx => {
-   await ctx.render('index', {
-       title: 'HELLO'
-   })
-});
+router.get('/', index);
+router.get('/add', login);
+
+async function index(ctx) {
+    await ctx.render('index', {
+        title: 'HELLO KOA',
+        msg
+    })
+}
+
+async function login(ctx) {
+    await ctx.render('add')
+}
 
 // 配置路由
 app.use(router.routes());
