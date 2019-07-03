f601428a1b78b4c47fea0fc8526f6807feb5c3a0
DingkeXue <605534423@qq.com>
2019-07-03 10:53:54 +0800

    评论管理

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router');
 const passport = require('koa-passport');
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 const validatePostInput = require('../../validation/post');
 const router = new Router();
 
@@ -64,7 +65,56 @@ router.get('/all', async ctx => {
 * */
 router.get('/', async ctx => {
     const post_id = ctx.query.id;
-    console.log(post_id);
+
+    await Post.findById(post_id)
+        .then(post => {
+            ctx.body = post;
+            ctx.status = 200;
+        })
+        .catch(err => {
+        err.nopost = '暂无任何评论';
+        ctx.body = nopost;
+        ctx.status = 200;
+    })
 });
+
+/*
+* @route DELETE api/posts?id=id
+* @desc 删除单个评论接口
+* @access 接口私有的
+* */
+router.delete('/',passport.authenticate('jwt', { session: false }), async ctx => {
+   const del_id = ctx.query.id;
+   const user_id = ctx.state.user.id;
+
+   // 查看该用户是否有详情
+    const profile = await Profile.find({user: user_id});
+
+    if (profile.length > 0) {
+        // 查找该用户留言
+        const post = await Post.findById(del_id);
+
+        // 判断是否本人操作
+        if (post.user.toString() !== user_id) {
+            ctx.status = 401;
+            ctx.body = '未授权';
+            return;
+        } else {
+            await Post.deleteOne({_id: del_id})
+                .then(() => {
+                ctx.status = 200;
+                ctx.body = {success: true}
+            })
+                .catch(err => {
+                    ctx.body = '删除时发生了一个错误';
+                    ctx.status = 400;
+                })
+        }
+    } else {
+        ctx.body = '该用户没有任何评论';
+        ctx.status = 404;
+    }
+});
+
 
 module.exports = router.routes();
